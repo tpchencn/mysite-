fa087ac31ee290894aa57c2830e4cce98139cc01
TAIMEI/tim.chen <tim.chen@mobilemd.cn>
2019-02-28 17:23:25 +0800

    add bootstrap

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse, Http404
+from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render
 import datetime
 
 
@@ -12,8 +15,13 @@ def my_homepage_view(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "It is now {0}".format(now)
-    return HttpResponse(html)
+    # html = "<html><body>It is now {0} </body></html>".format(now)
+    # t = get_template('current_datetime.html')
+    # t = Template("<html><body>It is now {{ current_date }} </body></html>")
+    context = {'current_date': now}
+    # html = t.render(context)
+    # return HttpResponse(html)
+    return render(request, 'current_datetime.html', context)
 
 
 def hours_ahead(request, offset):
@@ -22,5 +30,7 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    context = {"hour_offset": offset, "next_time": dt}
     html = "In {0} hour(s), it will be {1}".format(offset, dt)
-    return HttpResponse(html)
+    # return HttpResponse(html)
+    return render(request, "hours_ahead.html", context)
